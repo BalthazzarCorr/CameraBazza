48edec8fc6f448e7e3480078bae7d649426cec30
Peter <peev.petar@gmail.com>
2017-11-16 19:37:09 +0200

    Add project files.

---
+++ CameraBazza/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CameraBazza.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
