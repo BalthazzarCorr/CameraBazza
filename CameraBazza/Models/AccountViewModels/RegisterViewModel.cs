1413b95b39e420eca6b85e67b1e69ee48b5a7f04
Peter <peev.petar@gmail.com>
2017-11-18 19:35:30 +0200

    ads

--- CameraBazza/Models/AccountViewModels/RegisterViewModel.cs
+++ CameraBazza/Models/AccountViewModels/RegisterViewModel.cs
@@ -3,21 +3,31 @@
    using System.ComponentModel.DataAnnotations;
 
    public class RegisterViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+   {
+      [Required]
+      [StringLength(20, MinimumLength = 4)]
+      [RegularExpression(@"\w+", ErrorMessage = "User must have only letters")]
+      [Display(Name = "Username")]
+      public string Username { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+      [Required]
+      [EmailAddress]
+      [Display(Name = "Email")]
+      public string Email { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-    }
+      [Required]
+      [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+      [DataType(DataType.Password)]
+      [Display(Name = "Password")]
+      public string Password { get; set; }
+
+      [DataType(DataType.Password)]
+      [Display(Name = "Confirm password")]
+      [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+      public string ConfirmPassword { get; set; }
+
+      [Required]
+      [RegularExpression(@"\+\d{10,12}", ErrorMessage = "Phone must start with a '+' sign and contain between 10 and 12 symbols ")]
+      public string Phone { get; set; }
+   }
 }
--- CameraBazza/Startup.cs
+++ CameraBazza/Startup.cs
@@ -38,6 +38,8 @@
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+           app.UseDatabaseMigration();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
